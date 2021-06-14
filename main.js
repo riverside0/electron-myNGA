252d8626547c69a862a1ba28844016ac5c3d15ff
riverside0 <921818490@qq.com>
2021-06-14 17:17:30 +0800

    feat:增加icon 修复任务栏icon不显示问题

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+
 let electron = require("electron");
-
+const path = require('path')
 let app = electron.app;
-app.setAppUserModelId('myAppId')
+// app.setAppUserModelId('myAppId') //设置后会丢失任务栏icon
 let BrowserWindow = electron.BrowserWindow;
-
+let Tray = electron.Tray;
+let tray =null
 let mainWindow = null;
 
+
 app.on("ready", () => {
+  tray = new Tray(path.join(__dirname, 'nga.ico'))
+
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -15,6 +20,7 @@ app.on("ready", () => {
         contextIsolation: false,
         enableRemoteModule: true,
     },
+    icon: path.join(__dirname, 'nga.ico')
   });
   mainWindow.loadFile("index.html");
   mainWindow.on("closed", () => {
